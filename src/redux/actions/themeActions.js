0e4d5f80c1dc4bc899884aa943b61f7439d46da9
Sam'an <samangriffiths47@gmail.com>
2022-01-21 11:09:50 -0600

    html background

--- src/redux/actions/themeActions.js
+++ src/redux/actions/themeActions.js
@@ -1,9 +1,11 @@
 import { AUTHENTICATE, DARK_MODE, LIGHT_MODE, SEEN } from '../types'
 
 export function darkMode() {
+  document.querySelector('html').style.backgroundColor = 'black'
   return { type: DARK_MODE, payload: false }
 }
 export function lightMode() {
+  document.querySelector('html').style.backgroundColor = 'white'
   return { type: LIGHT_MODE, payload: true }
 }
 
